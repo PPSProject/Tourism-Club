df3c63be208e3a719614c590eaca9fccbdc9ac3e
Gryniam <muzikaeng@gmail.com>
2021-10-23 14:17:01 +0300

    DeletedComment

--- tourism club/Startup.cs
+++ tourism club/Startup.cs
@@ -33,7 +33,7 @@ namespace tourism_club
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
-                    //suka
+
                 });
             });
         }
