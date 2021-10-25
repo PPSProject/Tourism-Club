a34913bef4ad3d5decc3ad10cb211a0ce5a2e2f4
Gryniam <muzikaeng@gmail.com>
2021-10-25 16:56:35 +0300

    "Added all models"

---
+++ tourism club/Models/Admin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace tourism_club.Models
+{
+    public class Admin
+    {
+        [Key]
+        public int AdminId { get; set; }
+        public string Name { get; set; }
+        public string password { get; set; }
+        public string mail { get; set; }
+    }
+}
---
+++ tourism club/Models/Frame.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace tourism_club.Models
+{
+    public class Frame:Location
+    {
+        public string source { get; set; }
+        public string width { get; set; }
+        public string height { get; set; }
+        public string style { get; set; }
+        public string screen { get; set; }
+        public string loading { get; set; }
+    }
+}
